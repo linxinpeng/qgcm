1d118800c7cd84be7439dca81234b16bba0df820
linxinpeng <554085340@qq.com>
2020-07-12 14:40:31 +0800

    首页初稿完成

---
+++ data/data.js
@@ -0,0 +1,172 @@
+const data = {
+    goodList:[
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '123234245',
+                phone: '15937482323',
+                zytype: '商品资商品资源商品资源商品资源源',
+                account: '商商品资源商品资源品资源'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        },
+        {
+            userImg: 'images/user.jpg',
+            name:'王大全',
+            tag: ['商务','广州'],
+            company:'青岛海米有限公司',
+            desc: '专注供应链带货合作。我能提供的：国内外优质工厂祸患，上百种可以合作的卖货啊啥的产品',
+            sendTime: '2020.06.12',
+            userInfo:{
+                wechat: '372374245',
+                phone: '15937482323',
+                zytype: '商品资源',
+                account: '服装'
+            }
+        }
+    ]
+}
