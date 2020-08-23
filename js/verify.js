298b9525b36c2e5e53af723900617cda5318b47e
linxinpeng <554085340@qq.com>
2020-08-23 18:32:06 +0800

    添加验证等等

---
+++ js/verify.js
@@ -0,0 +1,49 @@
+layui.use(['form'], function () {
+    var form = layui.form;
+    form.verify({
+
+        title: function (value, item) {
+            if (!value) {
+                return '请输入标题！'
+            }
+        },
+        time: function(val){
+            if(!val){
+                return '请选择时间！'
+            }
+        },
+        wechat: function(val){
+            if(!val){
+                return '请输入微信号！'
+            }
+        },
+
+        qq:function(val){
+            if(!val){
+                return '请输入QQ号！'
+            }
+            if(!/^[1-9][0-9]{4,10}$/.test(val)){
+                return '请输入正确得QQ号码。'
+            }
+        },
+
+        username: function (value, item) { //value：表单的值、item：表单的DOM对象
+            if (!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)) {
+                return '用户名不能有特殊字符';
+            }
+            if (/(^\_)|(\__)|(\_+$)/.test(value)) {
+                return '用户名首尾不能出现下划线\'_\'';
+            }
+            if (/^\d+\d+\d$/.test(value)) {
+                return '用户名不能全为数字';
+            }
+        }
+
+        //我们既支持上述函数式的方式，也支持下述数组的形式
+        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+        , pass: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ]
+    })
+})
